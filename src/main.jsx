b9c79b4f74af001ecfe4b053f50dfb0dd75b6ac9
adjelritmo <renionapp@gmail.com>
2025-04-17 00:01:35 +0100

    configs para deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
-import Home from './screens/Home.jsx'
-import Splash from './screens/Splash.jsx'
 import Calculator from './screens/calculate.jsx'
 
 
