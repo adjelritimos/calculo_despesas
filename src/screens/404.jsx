ceb4562b8e6533605299c31bdf7572983acc7b08
adjelritmo <renionapp@gmail.com>
2025-04-16 22:49:47 +0100

    configs para deploy

--- src/screens/404.jsx
+++ src/screens/404.jsx
@@ -1,10 +1,11 @@
 function NotFound() {
     return (
-      <div>
-        <h1>404 - Página não encontrada</h1>
-        <p>A URL solicitada não foi encontrada neste servidor.</p>
-      </div>
+        <div class="container d-flex flex-column pt-5">
+            <h1 class="text-center fs-1 text-info mt-5">404</h1>
+            <h1 class="text-center fs-1 text-info">Página não encontrada</h1>
+            <i class="fas fa-exclamation-triangle text-center mt-5" style="font-size: 100px; color: orange;"></i>
+        </div>
     );
-  }
+}
 
 export default NotFound
